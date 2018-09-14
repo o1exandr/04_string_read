8bc246e525b88a260eede6bf591c6895b34c1e4f
olexandr.finyuk <olexandr.finyuk@gmail.com>
2018-09-14 20:32:45 +0300

    how does read char?

--- 04_string_read/Program.cs
+++ 04_string_read/Program.cs
@@ -1,8 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/*
+ 4. Написать программу, которая считывает символы с кла-
+виатуры(Console.Read()), пока не будет введена точка. Программа должна
+сосчитать количество введенных пользователем пробелов, 
+количество цифр, букв, знаков(char.IsDigit(), char.IsLetter(), ...). 
+ */
+
+
+using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
 namespace _04_string_read
 {
@@ -10,6 +18,15 @@ namespace _04_string_read
     {
         static void Main(string[] args)
         {
+            Console.Write("Input chars and last char enter point:\t");
+            string line = Console.ReadLine();
+            char chars;// = Console.ReadLine();
+            while (Console.Read() != '.')
+            {
+                chars = Console.Read();
+            }
+
+            //Console.ReadLine();
         }
     }
 }
